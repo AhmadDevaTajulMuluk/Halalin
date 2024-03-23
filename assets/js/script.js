a23e7463a3288b963995a18f39d9026f91c15beb
fadhlyrafi <rafiabang@gmail.com>
2024-03-23 12:37:56 +0700

    article, bug efek navbar
    Perbaikan bug efek navbar, waktu ditambah fetch dari essentials, javascript gak bisa ngambil elemen headernya, jadi ada sedikit perubahan, akhirnya bisa.
    
    Di artikel ada penambahan fitur, jadi maksimal baris yang di show di artikel hanya maksimal 3 baris, jika lebih akan diakhiri ...

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,24 +1,40 @@
-document.addEventListener('DOMContentLoaded', function() {
- 
-    fetch('../essentials/navbar.html')
-        .then(response => response.text())
-        .then(data => {
+document.addEventListener('DOMContentLoaded', function () {
+	fetch('../essentials/navbar.html')
+		.then((response) => response.text())
+		.then((data) => {
+			document.getElementById('navbar').innerHTML = data;
+		})
+		.catch((error) => console.error('Ada kesalahan:', error));
 
-            document.getElementById('navbar').innerHTML = data;
-        })
-        .catch(error => console.error('Ada kesalahan:', error));
-
-    fetch('../essentials/footer.html')
-        .then(response => response.text())
-        .then(data => {
-            document.getElementById('footer').innerHTML = data;
-        })
-        .catch(error => console.error('Ada kesalahan:', error));
+	fetch('../essentials/footer.html')
+		.then((response) => response.text())
+		.then((data) => {
+			document.getElementById('footer').innerHTML = data;
+		})
+		.catch((error) => console.error('Ada kesalahan:', error));
 });
 
 window.onload = function () {
-	const containerEffects = document.querySelectorAll('.container-effect');
+	const headerEffect = document.querySelector('.header-effect');
+	window.addEventListener('scroll', () => {
+		console.log('berhasil');
+		const elementBottom = headerEffect.offsetTop + headerEffect.offsetHeight;
+		if (window.scrollY > elementBottom) {
+			headerEffect.style.position = 'fixed';
+			headerEffect.style.top = '0px';
+			headerEffect.style.animation = 'fadeInDown 1s'; // Menggunakan properti animation untuk mendefinisikan animasi
+			headerEffect.style.backgroundColor = '#ffffff';
+			headerEffect.style.zIndex = '100';
+			headerEffect.style.width = '100%';
+		} else {
+			headerEffect.style.position = ''; // Menghapus properti position ketika kembali ke posisi asal
+			headerEffect.style.top = '';
+			headerEffect.style.animation = '';
+			headerEffect.style.zIndex = 'auto';
+		}
+	});
 
+	const containerEffects = document.querySelectorAll('.container-effect');
 	containerEffects.forEach((element) => {
 		window.addEventListener('scroll', () => {
 			const elementTop = element.offsetTop;
@@ -34,22 +50,5 @@ window.onload = function () {
 				element.style.transition = '0.5s ease-in-out';
 			}
 		});
-	});
-
-	const headerEffect = document.querySelector('.header-effect');
-	window.addEventListener('scroll', () => {
-		const elementBottom = headerEffect.offsetTop + headerEffect.offsetHeight;
-		if (window.scrollY > elementBottom) {
-			headerEffect.style.position = 'fixed';
-			headerEffect.style.top = '0px';
-			headerEffect.style.animation = 'fadeInDown 1s'; // Menggunakan properti animation untuk mendefinisikan animasi
-			headerEffect.style.backgroundColor = '#ffffff';
-			headerEffect.style.zIndex = '100';
-		} else {
-			headerEffect.style.position = ''; // Menghapus properti position ketika kembali ke posisi asal
-			headerEffect.style.top = '';
-			headerEffect.style.animation = '';
-			headerEffect.style.zIndex = 'auto';
-		}
 	});
 };
