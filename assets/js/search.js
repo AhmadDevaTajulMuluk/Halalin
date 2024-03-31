72735c2869cbcfe1f1fc1be377e69549e8ce2f1b
ahmaddevatajulm <ahmaddevatajulmuluk@gmail.com>
2024-04-01 06:43:14 +0700

    popup search

--- assets/js/search.js
+++ assets/js/search.js
@@ -1,7 +1,8 @@
-var dummyData = [
+let dummyData = [
   {
     name: "Asep Saepulloh",
     age: 30,
+    quran: "15 Juz",
     education: "Sarjana",
     location: "Garut",
     occupation: "Mekanik TikTok",
@@ -17,10 +18,11 @@ var dummyData = [
   {
     name: "Dadang Roller",
     age: 25,
-    education: "Sekolah Menegah Atas",
+    quran: "30 Juz",
+    education: "Sekolah Menengah Atas",
     location: "Majalengka",
     occupation: "Joki Balap Liar",
-    maritalStatus: "Belum Nikah",
+    maritalStatus: "Belum Menikah",
     personality: "Humoris",
     ethnicity: "Sunda",
     bodyType: "Kurus",
@@ -32,6 +34,7 @@ var dummyData = [
   {
     name: "Salman Kiprok",
     age: 35,
+    quran: "10 Juz",
     education: "Sarjana",
     location: "Bandung",
     occupation: "Manajer Bank",
@@ -47,10 +50,11 @@ var dummyData = [
   {
     name: "Agus Jumatan",
     age: 28,
-    education: "Sekolah Menegah Pertama",
+    quran: "5 Juz",
+    education: "Sekolah Menengah Pertama",
     location: "Tasikmalaya",
     occupation: "Guru Ngaji",
-    maritalStatus: "Belum Nikah",
+    maritalStatus: "Belum Menikah",
     personality: "Penyabar",
     ethnicity: "Sunda",
     bodyType: "Kurus",
@@ -62,18 +66,18 @@ var dummyData = [
 ];
 
 function search() {
-  var searchType = document.getElementById("searchType").value;
-  var resultsContainer = document.getElementById("results");
+  let searchType = document.getElementById("searchType").value;
+  let resultsContainer = document.getElementById("results");
   resultsContainer.innerHTML = "";
 
   if (searchType === "physical") {
-    var bodyType = document.getElementById("bodyType").value.toLowerCase();
-    var skinColor = document.getElementById("skinColor").value.toLowerCase();
-    var hairColor = document.getElementById("hairColor").value.toLowerCase();
-    var weight = parseFloat(document.getElementById("weight").value);
-    var height = parseFloat(document.getElementById("height").value);
+    let bodyType = document.getElementById("bodyType").value.toLowerCase();
+    let skinColor = document.getElementById("skinColor").value.toLowerCase();
+    let hairColor = document.getElementById("hairColor").value.toLowerCase();
+    let weight = parseFloat(document.getElementById("weight").value);
+    let height = parseFloat(document.getElementById("height").value);
 
-    var physicalResults = dummyData.filter(function (person) {
+    let physicalResults = dummyData.filter(function (person) {
       return (
         (!bodyType || person.bodyType.toLowerCase() === bodyType) &&
         (!skinColor || person.skinColor.toLowerCase() === skinColor) &&
@@ -85,21 +89,23 @@ function search() {
 
     displayResults(physicalResults);
   } else if (searchType === "nonPhysical") {
-    var age = parseInt(document.getElementById("age").value);
-    var education = document.getElementById("education").value.toLowerCase();
-    var location = document.getElementById("location").value.toLowerCase();
-    var occupation = document.getElementById("occupation").value.toLowerCase();
-    var maritalStatus = document
+    let age = parseInt(document.getElementById("age").value);
+    let quran = document.getElementById("quran").value.toLowerCase();
+    let education = document.getElementById("education").value.toLowerCase();
+    let location = document.getElementById("location").value.toLowerCase();
+    let occupation = document.getElementById("occupation").value.toLowerCase();
+    let maritalStatus = document
       .getElementById("maritalStatus")
       .value.toLowerCase();
-    var personality = document
+    let personality = document
       .getElementById("personality")
       .value.toLowerCase();
-    var ethnicity = document.getElementById("ethnicity").value.toLowerCase();
+    let ethnicity = document.getElementById("ethnicity").value.toLowerCase();
 
-    var nonPhysicalResults = dummyData.filter(function (person) {
+    let nonPhysicalResults = dummyData.filter(function (person) {
       return (
         (isNaN(age) || person.age === age) &&
+        (!quran || person.quran.toLowerCase() === quran) &&
         (!education || person.education.toLowerCase() === education) &&
         (!location || person.location.toLowerCase() === location) &&
         (!occupation || person.occupation.toLowerCase() === occupation) &&
@@ -114,69 +120,17 @@ function search() {
   }
 }
 
-function displayResults(results) {
-  var resultsContainer = document.getElementById("results");
-  var notFound = document.getElementById("notFound");
-  resultsContainer.innerHTML = ""; // Bersihkan hasil sebelumnya
-
-  if (results.length === 0) {
-    notFound.style.display = "block";
-    return;
-  } else {
-    notFound.style.display = "none";
-  }
-
-  results.forEach(function (person) {
-    var card = document.createElement("div");
-    card.classList.add("card");
-
-    var content = `
-          <div class="card-header">
-            <h3>${person.name}</h3>
-            <p>Umur: ${person.age}</p>
-          </div>
-          <div class="card-body">
-            <p><strong>Pendidikan:</strong> ${person.education}</p>
-            <p><strong>Domisili:</strong> ${person.location}</p>
-            <p><strong>Pekerjaan:</strong> ${person.occupation}</p>
-            <p><strong>Status Pernikahan:</strong> ${person.maritalStatus}</p>
-            <p><strong>Sifat:</strong> ${person.personality}</p>
-            <p><strong>Suku Bangsa:</strong> ${person.ethnicity}</p>
-        `;
-
-    if (person.bodyType) {
-      content += `<p><strong>Bentuk Fisik:</strong> ${person.bodyType}</p>`;
-    }
-    if (person.skinColor) {
-      content += `<p><strong>Warna Kulit:</strong> ${person.skinColor}</p>`;
-    }
-    if (person.hairColor) {
-      content += `<p><strong>Warna rambut:</strong> ${person.hairColor}</p>`;
-    }
-    if (person.weight) {
-      content += `<p><strong>Berat Badan:</strong> ${person.weight}</p>`;
-    }
-    if (person.height) {
-      content += `<p><strong>Tinggi Badan:</strong> ${person.height}</p>`;
-    }
-
-    content += `</div>`; // Tutup card-body
-
-    card.innerHTML = content;
-    resultsContainer.appendChild(card);
-  });
-}
 
 document.addEventListener("DOMContentLoaded", function () {
-  var searchType = document.getElementById("searchType");
+  let searchType = document.getElementById("searchType");
   searchType.value = "physical"; // Set default searchType to 'physical'
   searchType.dispatchEvent(new Event("change")); // Trigger change event to update UI
 });
 
 document.getElementById("searchType").addEventListener("change", function () {
-  var searchType = this.value;
-  var physicalSearch = document.getElementById("physicalSearch");
-  var nonPhysicalSearch = document.getElementById("nonPhysicalSearch");
+  let searchType = this.value;
+  let physicalSearch = document.getElementById("physicalSearch");
+  let nonPhysicalSearch = document.getElementById("nonPhysicalSearch");
 
   if (searchType === "physical") {
     physicalSearch.style.display = "block";
@@ -187,49 +141,116 @@ document.getElementById("searchType").addEventListener("change", function () {
   }
 });
 
-// bingung banh masih belum bisa
-function displayProfile() {
-  var card = document.createElement("div");
-  card.classList.add("card");
+function displayResults(results) {
+  let resultsContainer = document.getElementById("results");
+  let notFound = document.getElementById("notFound");
+  resultsContainer.innerHTML = ""; // Clear previous results
 
-  var person = dummyData[0]; // Ambil data dari index pertama (0)
+  if (results.length === 0) {
+    notFound.style.display = "block";
+    return;
+  } else {
+    notFound.style.display = "none";
+  }
 
-  var profileContent = `
-        <div class="profile-header">
-          <h2>Profil</h2>
-        </div>
-        <div class="profile-body">
+  results.forEach(function (person) {
+    let card = document.createElement("div");
+    card.classList.add("card");
+
+    let content = `
+        <div class="card-header">
           <h3>${person.name}</h3>
-          <p><strong>Umur:</strong> ${person.age}</p>
+          <p>Umur: ${person.age}</p>
+        </div>
+        <div class="card-body">
+          <p><strong>Hafalan:</strong> ${person.quran}</p>
           <p><strong>Pendidikan:</strong> ${person.education}</p>
           <p><strong>Domisili:</strong> ${person.location}</p>
           <p><strong>Pekerjaan:</strong> ${person.occupation}</p>
-          <p><strong>Status Pernikahan:</strong> ${person.maritalStatus}</p>
+          <p><strong>Status:</strong> ${person.maritalStatus}</p>
           <p><strong>Sifat:</strong> ${person.personality}</p>
           <p><strong>Suku Bangsa:</strong> ${person.ethnicity}</p>
       `;
 
+    if (person.bodyType) {
+      content += `<p><strong>Bentuk Fisik:</strong> ${person.bodyType}</p>`;
+    }
+    if (person.skinColor) {
+      content += `<p><strong>Warna Kulit:</strong> ${person.skinColor}</p>`;
+    }
+    if (person.hairColor) {
+      content += `<p><strong>Warna Rambut:</strong> ${person.hairColor}</p>`;
+    }
+    if (person.weight) {
+      content += `<p><strong>Berat Badan:</strong> ${person.weight}</p>`;
+    }
+    if (person.height) {
+      content += `<p><strong>Tinggi Badan:</strong> ${person.height}</p>`;
+    }
+
+    content += `</div>`; // Close card-body
+
+    card.innerHTML = content;
+    resultsContainer.appendChild(card);
+
+    // Add click event listener to each card to display popup
+    card.addEventListener("click", function () {
+      displayPopup(person);
+    });
+  });
+}
+
+function displayPopup(person) {
+  // Create popup container
+  let popupBackdrop = document.getElementById("popupBackdrop");
+  popupBackdrop.style.display = "block";
+
+  let popupContent = `
+    <div class="popup-search">
+      <h2>${person.name}</h2>
+      <p><strong>Umur:</strong> ${person.age}</p>
+      <p><strong>Hafalan:</strong> ${person.quran}</p>
+      <p><strong>Pendidikan:</strong> ${person.education}</p>
+      <p><strong>Domisili:</strong> ${person.location}</p>
+      <p><strong>Pekerjaan:</strong> ${person.occupation}</p>
+      <p><strong>Status:</strong> ${person.maritalStatus}</p>
+      <p><strong>Sifat:</strong> ${person.personality}</p>
+      <p><strong>Suku Bangsa:</strong> ${person.ethnicity}</p>
+  `;
+
   if (person.bodyType) {
-    profileContent += `<p><strong>Bentuk Fisik:</strong> ${person.bodyType}</p>`;
+    popupContent += `<p><strong>Bentuk Fisik:</strong> ${person.bodyType}</p>`;
   }
   if (person.skinColor) {
-    profileContent += `<p><strong>Warna Kulit:</strong> ${person.skinColor}</p>`;
+    popupContent += `<p><strong>Warna Kulit:</strong> ${person.skinColor}</p>`;
   }
   if (person.hairColor) {
-    profileContent += `<p><strong>Warna rambut:</strong> ${person.hairColor}</p>`;
+    popupContent += `<p><strong>Warna Rambut:</strong> ${person.hairColor}</p>`;
   }
   if (person.weight) {
-    profileContent += `<p><strong>Berat Badan:</strong> ${person.weight}</p>`;
+    popupContent += `<p><strong>Berat Badan:</strong> ${person.weight}</p>`;
   }
   if (person.height) {
-    profileContent += `<p><strong>Tinggi Badan:</strong> ${person.height}</p>`;
+    popupContent += `<p><strong>Tinggi Badan:</strong> ${person.height}</p>`;
   }
 
-  profileContent += `</div>`; // Close profile-body
+  popupContent += `
+    <button id="closePopup">Close</button>
+    <a href="#" class="cv-button" id="cv-button">Lihat CV</a>
+  </div>
+  
+  `;
+  
 
-  profileContainer.innerHTML = profileContent;
-  card.innerHTML = content;
+  popupBackdrop.innerHTML = popupContent;
+
+  // Add event listener to close popup when close button is clicked
+  let closePopupButton = document.getElementById("closePopup");
+  closePopupButton.addEventListener("click", function () {
+    popupBackdrop.style.display = "none";
+  });
 }
-document.addEventListener("DOMContentLoaded", function () {
-  displayProfile();
+
+document.getElementById("cv-button").addEventListener("click", function() {
+  window.location.href = "#"; 
 });
